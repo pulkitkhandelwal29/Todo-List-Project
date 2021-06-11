013fc1327001fd5cafe939947d9a460884a5f8eb
pulkitkhandelwal29 <pulkit.khandelwal29@gmail.com>
2021-06-11 22:47:43 +0530

    Created Todo Model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Todo
+
+#There is field named 'created' in models
+#Django Admin does not show it as it is automatically taken
+#We show using this and reading as readonly_fields so that no one can edit it
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
 # Register your models here.
+admin.site.register(Todo,TodoAdmin)
