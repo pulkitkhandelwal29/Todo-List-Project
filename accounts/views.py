7b286e3dfa63456a81825ee52598e88840089f0f
pulkitkhandelwal29 <pulkit.khandelwal29@gmail.com>
2021-06-11 18:29:16 +0530

    Added Sign Up function

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+#Helps to create predefined forms (signup form--username,password1,password2)
+from django.contrib.auth.forms import UserCreationForm
+
+#Create user
+from django.contrib.auth.models import User
+#Helps to login
+from django.contrib.auth import login
+
+#Integrity lets you uniquely identify column in database, if not uniquely identify IntegrityError is returned
+from django.db import IntegrityError
+
 
 # Create your views here.
+def signup(request):
+    #If signup link is opened, it's a GET request
+    if request.method == 'GET':
+        return render(request,'accounts/signup.html',{'form':UserCreationForm()})
+    else:
+        #Otherwise the request is POST(posting the details and clicking on sign up button)
+        #Retrieving details
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if  password1 == password2:
+            # Create a new user
+            try:
+                user = User.objects.create_user(username = username , password = password1)
+                #Saving user into database
+                user.save()
+                #As soon as user signs up, it is logged in
+                login(request,user)
+                #After logging in, we need to show some page (currenttodos.html),redirecting to webpage
+                return redirect('currenttodos')
+
+            except IntegrityError:
+                return render(request,'accounts/signup.html',{'form':UserCreationForm(),'error':'Username already exists!'})
+        else:
+            #If passwords does not match (return the same page again with additional details)
+            return render(request,'accounts/signup.html',{'form':UserCreationForm(),'error':'Passwords didn\'t match'})
--- todo_list/urls.py
+++ todo_list/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    #Authentication
+    path('accounts/',include('accounts.urls')),
+
+    #todos
+    path('todo/',include('todo.urls')),
 ]
